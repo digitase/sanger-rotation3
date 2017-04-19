6f4bf807fd4932acd4c40c2be428f1a826a3949a
Ben Bai (digitase) <bb9@sanger.ac.uk>
2017-04-19 15:52:02 +0100

    Adds .embl inputs to 2_reportlabtest.sh

--- src/2_homoplasy/2_reportlabtest.sh
+++ src/2_homoplasy/2_reportlabtest.sh
@@ -10,6 +10,12 @@ declare -a tabs=(
     "/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/2_homoplasy/reconstruct_snps_on_tree/acctran/S.aureus_ST30_BSAC_Pfizer/S.aureus_ST30_BSAC_Pfizer_homoplasies_on_tree.tab" 
     "/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/2_homoplasy/reconstruct_snps_on_tree/acctran/S.aureus_ST8_BSAC_Pfizer_revised/S.aureus_ST8_BSAC_Pfizer_revised_homoplasies_on_tree.tab" 
 )
+declare -a embls=(
+    '/lustre/scratch118/infgen/team81/dj9/staph.aureus/ben/st22/CC22_EMRSA15.embl' \
+    '/lustre/scratch118/infgen/team81/dj9/staph.aureus/ben/st239/CC8_TW20.embl' \
+    '/lustre/scratch118/infgen/team81/dj9/staph.aureus/ben/st30/CC30_MRSA252.embl' \
+    '/lustre/scratch118/infgen/team81/dj9/staph.aureus/ben/st8/CC8_USA300_FPR3757.embl'
+)
 # acctran trees
 declare -a trees=(
     "/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/2_homoplasy/reconstruct_snps_on_tree/acctran/S.aureus_ST22_BSAC_Pfizer/S.aureus_ST22_BSAC_Pfizer_acctran_steps.tre" 
@@ -30,8 +36,10 @@ declare -a prefixes=(
 #containing headings for each column of data. The metadata file is provided with
 #the -m option. 
 
+                # -b 2941669 -e 2944488 \
 for (( i = 0; i < ${#trees[@]}; i++ )); do
     tree="${trees[$i]}"
+    embl="${embls[$i]}"
     prefix="${prefixes[$i]}"
     tab="${tabs[$i]}"
     # Make st specific output dir
@@ -46,12 +54,13 @@ for (( i = 0; i < ${#trees[@]}; i++ )); do
         -J "reportlabtest.$prefix" \
         -o "$outdir/jobid_%J.bsub_o.log" \
         -e "$outdir/jobid_%J.bsub_e.log" \
-            python2 ~sh16/scripts/reportlabtest.py \
+            python2 /nfs/users/nfs_b/bb9/workspace/rotation3/src/2_homoplasy/reportlabtest_modified.py \
                 -t "$tree" \
                 -q taxa \
+                -l 2 -A 2 -E 5 \
                 -a 2 -L left \
                 -o "$prefix.homoplasies_on_acctran_steps_tree.pdf" \
-                "$tab" 
+                "$tab" "$embl"
     echo Finished $prefix at $(date)
 done
     
