80445336546d9c39043476e3795227b0132465a1
Ben Bai (digitase) <bb9@sanger.ac.uk>
2017-05-02 16:14:53 +0100

    Adds script to find homoplasies at the same site in multiple STs.

---
+++ src/3_find_candidates/2.0_compare_strains_by_site.py
@@ -0,0 +1,48 @@
+'''Compare homoplasic sites across strains
+
+Identify sites where homoplasies have occured in multiple strains
+'''
+
+import pandas as pd
+
+prefixes = {
+    "st22": "S.aureus_ST22_BSAC_Pfizer",
+    "st239": "S.aureus_ST239_global_Singapore_Pfizer",
+    "st30": "S.aureus_ST30_BSAC_Pfizer",
+    "st8": "S.aureus_ST8_BSAC_Pfizer_revised"
+}
+hp_file_template = "/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/2_homoplasy/summarise_homoplasies/{prefix}/{short_prefix}_homoplasies.csv"
+embl_files = {
+    "st22": "/nfs/users/nfs_b/bb9/workspace/rotation3/data/st22/CC22_EMRSA15.embl",
+    "st239": "/nfs/users/nfs_b/bb9/workspace/rotation3/data/st239/CC8_TW20.embl",
+    "st30": "/nfs/users/nfs_b/bb9/workspace/rotation3/data/st30/CC30_MRSA252.embl",
+    "st8": "/nfs/users/nfs_b/bb9/workspace/rotation3/data/st8/CC8_USA300_FPR3757.embl",
+}
+snps_file_template = "/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/2_homoplasy/summarise_snps/{prefix}/{prefix}.out"
+
+print('Reading hps...')
+hps = dict((short_prefix, pd.read_csv(hp_file_template.format(prefix=prefixes[short_prefix], short_prefix=short_prefix))) for short_prefix in prefixes)
+print('Reading snp summaries...')
+snps = dict((short_prefix, pd.read_table(snps_file_template.format(prefix=prefixes[short_prefix], short_prefix=short_prefix))) for short_prefix in prefixes)
+
+print('Mapping st loc to alignment loc...')
+pos_to_alignment_pos = {}
+for short_prefix in prefixes:
+    #  short_prefix = 'st22'
+    print(short_prefix)
+    hp = hps[short_prefix]
+    snp = snps[short_prefix]
+    pos_to_alignment_pos[short_prefix] = dict(zip(snp.iloc[:, 1], snp.iloc[:, 0]))
+    hp['loc_in_alignment'] = hp['loc'].apply(lambda loc: pos_to_alignment_pos[short_prefix][loc])
+
+print('Merging st datasets...')
+hps_merged = pd.concat(hps.values(), keys=hps.keys()).reset_index()
+
+# Filter out non-homplasic sites
+print('Filtering out non homoplasic sites...')
+hps_merged = hps_merged.loc[hps_merged['n_homoplasic_acctran'].astype(bool) | hps_merged['n_homoplasic_deltran'].astype(bool)]
+
+print('Writing output...')
+dupe_sites = hps_merged[hps_merged.duplicated('loc_in_alignment', keep=False)].sort_values('loc_in_alignment')
+dupe_sites.to_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/3_find_candidates/hp_sites_summary.csv')
+
