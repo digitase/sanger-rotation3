80b94a00f67510d4ad132110a4dbd555007509ef
Ben Bai (digitase) <bb9@sanger.ac.uk>
2017-05-10 17:05:03 +0100

    Adds output of unannotated loci

--- src/4_verify_candidates/2.0_filter_candidates.py
+++ src/4_verify_candidates/2.0_filter_candidates.py
@@ -14,7 +14,7 @@ def flatten_list_or_tuple(xs):
 
 def tag_known_loci(candidates, known_amr_loci):
     for gs in candidates:
-        gs = flatten_list_or_tuple(eval(gs))
+        gs = flatten_list_or_tuple(gs)
         gs_result = set()
         for g in gs:
             if g:
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     with open('.output/known_amr_loci.txt', 'r') as infile:
         known_amr_loci = [x.strip().lower() for x in infile.readlines()]
 
-    summary_sites = pd.read_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/3_find_candidates/hp_sites_summary.csv')
+    summary_sites = pd.read_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/3_find_candidates/hp_sites_summary.csv', converters={'gene': eval, 'locus_tag': eval, 'product': eval})
     summary_sites['known_amr_loci'] = list(tag_known_loci(summary_sites['gene'], known_amr_loci))
     summary_sites_by_gene = pd.DataFrame().assign(
         sts=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['level_0'])),
@@ -38,18 +38,18 @@ if __name__ == "__main__":
         n_homoplasic_acctran=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['n_homoplasic_acctran'])),
         n_homoplasic_deltran=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['n_homoplasic_deltran'])),
         n_total=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['n_total_acctran'])),
-        locus_tag=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['locus_tag'])),
-        product=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['product'])),
-        gene=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['gene'])),
+        locus_tags=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['locus_tag'])),
+        products=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['product'])),
+        genes=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['gene'])),
         intergenic=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['intergenic'])),
         known_amr_loci=summary_sites.groupby('loc_in_alignment').apply(lambda x: tuple(x['known_amr_loci']))
     )
 
-    summary_genic = pd.read_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/3_find_candidates/hp_homologs_summary_genic.csv')
+    summary_genic = pd.read_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/3_find_candidates/hp_homologs_summary_genic.csv', converters={'genes': eval, 'homologs': eval, 'products': eval})
     summary_genic = summary_genic.query('n_sts > 1')
     summary_genic['known_amr_loci'] = list(tag_known_loci(summary_genic['genes'], known_amr_loci))
 
-    summary_intergenic = pd.read_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/3_find_candidates/hp_homologs_summary_intergenic.csv')
+    summary_intergenic = pd.read_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/3_find_candidates/hp_homologs_summary_intergenic.csv', converters={'genes': eval, 'homologs': eval, 'products': eval})
     summary_intergenic = summary_intergenic.query('n_sts > 1')
     summary_intergenic['known_amr_loci'] = list(tag_known_loci(summary_intergenic['genes'], known_amr_loci))
 
@@ -61,4 +61,39 @@ if __name__ == "__main__":
     summary_sites_by_gene_filtered.to_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/4_verify_candidates/hp_sites_summary.filtered.csv')
     summary_genic_filtered.to_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/4_verify_candidates/hp_homologs_summary_genic.filtered.csv')
     summary_intergenic_filtered.to_csv('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/4_verify_candidates/hp_homologs_summary_intergenic.filtered.csv')
+
+    # Output non-annotated locus tags i.e. no gene name OR putative or hypothetical in products
+    def add_unannotated_locus_tags(gss, pss, lss, existing_results=None):
+        '''Add locus tags without annotation to a set
+        '''
+        if not existing_results:
+            existing_results = set()
+        for gs, ps, ls in zip(gss, pss, lss):
+            if all(g == None for g in gs):
+                existing_results.update(ls)
+            for p, l in zip(ps, ls):
+                if 'hypothetical' in p or 'putative' in p:
+                    existing_results.add(l)
+        return(existing_results)
+    #
+    locus_tags_unannotated = add_unannotated_locus_tags(
+        summary_sites_by_gene_filtered['genes'].map(lambda x: list(flatten_list_or_tuple(x))),
+        summary_sites_by_gene_filtered['products'].map(lambda x: list(flatten_list_or_tuple(x))),
+        summary_sites_by_gene_filtered['locus_tags'].map(lambda x: list(flatten_list_or_tuple(x)))
+    )
+    locus_tags_unannotated = add_unannotated_locus_tags(
+        summary_genic_filtered['genes'].map(lambda x: list(flatten_list_or_tuple((x)))),
+        summary_genic_filtered['products'].map(lambda x: list(flatten_list_or_tuple((x)))),
+        summary_genic_filtered['homologs'].map(lambda x: list(flatten_list_or_tuple((x)))),
+        locus_tags_unannotated
+    )
+    locus_tags_unannotated = add_unannotated_locus_tags(
+        summary_intergenic_filtered['genes'].map(lambda x: list(flatten_list_or_tuple((x)))),
+        summary_intergenic_filtered['products'].map(lambda x: list(flatten_list_or_tuple((x)))),
+        summary_intergenic_filtered['homologs'].map(lambda x: list(flatten_list_or_tuple((x)))),
+        locus_tags_unannotated
+    )
+
+    with open('/nfs/users/nfs_b/bb9/workspace/rotation3/lustre/4_verify_candidates/locus_tags_unannotated.txt', 'w') as outfile:
+        outfile.write('\n'.join(locus_tags_unannotated))
 
