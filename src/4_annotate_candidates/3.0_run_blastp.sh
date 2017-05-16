645c73f3e57dd97fd7dfcc891394e90eaa1a60ad
Ben Bai (digitase) <bb9@sanger.ac.uk>
2017-05-16 16:37:49 +0100

    Adds blast_formatter conversion of output to tabular

--- src/4_annotate_candidates/3.0_run_blastp.sh
+++ src/4_annotate_candidates/3.0_run_blastp.sh
@@ -1,23 +1,40 @@
 
 # blastp against nr protein db
 
+# input_pep_fa=".output/interproscan/st_all.cds.pep.unannotated.test.fasta"
 input_pep_fa=".output/interproscan/st_all.cds.pep.unannotated.fasta"
 blastdb_nr='/data/blastdb/Supported/NR/nr'
 outdir=".output/blastp"
 
 mkdir -p "$outdir"
 
+outfmt=11
+nthreads=4
+outblastpfile="$outdir/st_all.cds.pep.unannotated.blastp.outfmt$outfmt"
+
 bsub \
     -G team81 -q long \
-    -n 4 -R "select[mem>4000] rusage[mem=4000]" -M 4000 \
+    -n "$nthreads" -R "select[mem>24000] rusage[mem=24000] span[hosts=1]" -M 24000 \
     -J "blastp_unannotated" \
     -o "$outdir/jobid_%J.bsub_o.log" \
     -e "$outdir/jobid_%J.bsub_e.log" \
         blastp \
             -query "$input_pep_fa" \
             -db "$blastdb_nr" \
-            -out "$outdir/st_all.cds.pep.unannotated.blastp.outfmt6.tsv" \
-            -outfmt 6 \
+            -out "$outblastpfile" \
+            -outfmt "$outfmt" \
+            -num_threads "$nthreads" \
+            -max_target_seqs 3
+
+bsub \
+    -G team81 -q normal \
+    -R "select[mem>16000] rusage[mem=16000]" -M 16000 \
+    -J "blast_formatter_unannotated" \
+    -o "$outdir/jobid_%J.bsub_o.log" \
+    -e "$outdir/jobid_%J.bsub_e.log" \
+        blast_formatter \
+            -archive "$outblastpfile" \
+            -outfmt "6 qseqid sseqid stitle pident length mismatch gapopen qstart qend sstart send evalue bitscore" \
             -max_target_seqs 1 \
-            -num_threads 4
+            -out "$outdir/st_all.cds.pep.unannotated.blastp.outfmt6cust"
 
