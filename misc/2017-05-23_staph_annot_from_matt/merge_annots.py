7e06be1a9f5c6d720d4f641d8d1d7208c0e150f5
Ben Bai (digitase) <bb9@sanger.ac.uk>
2017-05-24 15:34:47 +0100

    Adds locus tag and other qualifiers to tRNA and rRNAs for st22

--- misc/2017-05-23_staph_annot_from_matt/merge_annots.py
+++ misc/2017-05-23_staph_annot_from_matt/merge_annots.py
@@ -32,6 +32,35 @@ with open('./EMRSA15.art', 'r') as infile:
         elif line.startswith('FT                   /systematic_id="'):
             merged.append(line)
             merged.append(line.replace('systematic_id', 'locus_tag'))
+        #  FT                   /note="tRNA Leu anticodon GAG, Cove score 40.45"
+        elif line.startswith('FT                   /note="tRNA '):
+            aa, _, codon = line.split('"')[1].split()[1:4]
+            merged.append('FT                   /locus_tag="SAEMRSA15tRNA{}{}"\n'.format(aa, codon))
+            merged.append(line)
+        #  FT                   /gene="tRNA_47"
+        #  FT   tRNA            complement(1945407..1945478)
+        #  FT                   /domain="Rfam:RF00005;tRNA;Score=38.40;positions 1 to 59"
+        #  FT                   /gene="tRNA_50"
+        elif line.startswith('FT                   /gene="tRNA_'):
+            merged.append(line)
+            gene = line.split('"')[1]
+            merged.append('FT                   /locus_tag="SAEMRSA15tRNA{}"\n'.format(gene.replace('_', '')))
+            merged.append('FT                   /product="{}"\n'.format(gene))
+        elif line.startswith('FT   rRNA'):
+            merged.append(line)
+            #  FT   rRNA            complement(1951221..1952775)
+            #  FT                   /label=23S
+            loc_str = line.strip().split()[-1]
+            start, end = loc_str.replace('complement', '').replace('(', '').replace(')', '').split('..')
+            line = next(infile)
+            label = line.strip().replace('FT                   /label=', '')
+            if 'complement' in loc_str:
+                merged.append('FT                   /locus_tag="SAEMRSA15rRNA{}{}{}"\n'.format(label, start, end))
+            else:
+                merged.append('FT                   /locus_tag="SAEMRSA15rRNA{}{}{}"\n'.format(label, end, start))
+            merged.append('FT                   /product="{} rRNA st22 loc: {}"\n'.format(label, loc_str))
+            merged.append('FT                   /gene="{}"\n'.format(label))
+            merged.append(line)
         elif line.startswith('SQ'):
             add_ncRNAs(merged)
             merged.append(line)
